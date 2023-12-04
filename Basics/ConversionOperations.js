786658392ee67955e8af5b30ba351dfa9e336a5d
Farhan Anjum <your.email@example.com>
2023-12-04 09:44:21 +0500

    conversion of datatypes

---
+++ Basics/ConversionOperations.js
@@ -0,0 +1,36 @@
+let score = 33
+
+
+
+
+console.log(typeof(score)); //Number
+
+console.log(typeof score);
+
+// Conversion
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+
+// "33" to 33
+// "33BC" to NaN
+// true to 1 ; false to 0
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn) 
+
+
+
+
+let someNumber = 33
+
+let stringNumber = string(someNumber)
+console.log(stringNumber);
+
+
+
+
+
+
