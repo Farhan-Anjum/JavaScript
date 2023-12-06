a6193d4a0c92d37165ab409d525cfb56d6e3bb14
Farhan Anjum <your.email@example.com>
2023-12-06 15:36:00 +0500

    String conversion Done

--- Basics/ConversionOperations.js
+++ Basics/ConversionOperations.js
@@ -31,6 +31,55 @@ console.log(stringNumber);
 
 
 
+*********************************************************************************************************
+
+let str1 = "Hello"
+let str2 = " Farhan"
+console.log(str1 + str2); //Hello Farhan
+
+
+console.log("1" + 2); //12
+console.log(1 + "2"); //12
+
+console.log("1" + 2 + 2); //112
+
+console.log(1 + 2 + "2"); //32
+
+//Rule: Jb JS ka conversion ho - agr string first hai to usko consider kiya jaye ga
+//otherwise jo conversion wo hogi then string.
+
+
+//Advance Conversion
+
+console.log(true);
+console.log(+true); // 1
+console.log(true+) //through error
+
+console.log(+""); //Not preffered
+
+
+ let num1, num2, num3
+
+ num1 = num2 = num3 = 2 + 2;
+
+
+ let gameCounter = 100
+ gameCounter++;
+ //++gameCounter //Also Valid
+ //read prefix and postfix operators reference MDN Docs
+ //Type Conversion
+
+ console.log( gameCounter); //101
+
+
+
+
+
+
+
+
+
+
 
 
 
