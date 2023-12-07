8adaafb8e0a2f1b260b6412dfa7a08d758731a35
Farhan Anjum <your.email@example.com>
2023-12-07 14:55:44 +0500

    Comparison Operators Done

---
+++ Basics/Comparison.js
@@ -0,0 +1,35 @@
+//Basic Comaparison Operators
+
+console.log(2 > 1); //Greater than
+console.log(2 >= 1); //Greater than equal to
+console.log(2 < 1); //less than
+console.log( 2 == 1); // Comparison
+console.log( 2 != 1); // not equal
+
+
+
+
+console.log("2" > 1); //True
+
+console.log("02" > 1); //True
+
+//Avoid such comparison for ease. Only Clean Code.
+
+console.log(null > 0);  //
+console.log(null == 0); //
+console.log(null >= 0); //
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// Comparison and Equality check are two different things
+
+
+//Strict Check ===
+
+console.log("2" === 2); //not same
+
+
+
+
