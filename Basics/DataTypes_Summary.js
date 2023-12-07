ccea94aa168cfde03799f5a066de3f89a17d03df
Farhan Anjum <your.email@example.com>
2023-12-07 15:19:55 +0500

    DataTypes_Summary

---
+++ Basics/DataTypes_Summary.js
@@ -0,0 +1,64 @@
+//Primitive and Non Primitive
+
+//Primitive -  7 types:
+//Strings
+//Numbers
+//Boolean
+//Null / Empty : not zero
+//Undefined
+//Symbol to make value unique
+//BigInt
+
+//Examples:
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outSidetemp = null;
+let userEmail;
+
+const id = Symbol('123');
+const anotherId =Symbol("123");
+console.log(id === anotherID);
+
+const bigNumber = 12334589585958558955785477
+
+//Reference Type or Non-primitive
+
+//Arrays
+//Objects
+//Functions
+
+//Examples
+const heros = ["Superman", "Aquaman", "Spiderman"]
+
+
+let myObj = {
+    name: "Farhan",
+    age: 23,
+
+}
+
+
+const myFunction = function(){
+    console.log("Hello World")
+}
+
+
+//to find datatype
+console.log(typeof bigNumber); //
+
+
+//Non primitives has datatype of Functions
+
+
+
+
+
+
+
+
+
+
+
+
