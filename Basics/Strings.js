5e0af20335c1e700ae276a60d0fc1d673d8288dc
Farhan Anjum <your.email@example.com>
2023-12-10 09:26:52 +0500

    strings done

---
+++ Basics/Strings.js
@@ -0,0 +1,80 @@
+const name = "Farhan";
+
+const repCount = 50;
+
+//console.log(name + repCount + "Value"); 
+//not recommend method for concat
+
+
+//Back tick is preferred.
+
+console.log(`hello my  name is ${name} and my repo count is ${repoCount}`);
+
+//New way to declre a string ans givs key value pair in output
+const name = new string("Farhan")
+
+console.log(name[0]); //F will be the output
+
+
+
+//console.log(name.length);
+//console.log(name.toUpperCase());
+
+//console.log(name.charAt(2)); //r
+
+
+//console.log(name.indeOf(("a"));
+
+//Substring
+const newString = name.substring(0,4)
+console.log(newString);
+
+//Slice and neg can be used as well
+const anotherSring = name.slice(0, 4);
+console.log(anotherString);
+
+
+//Trim to remove space and some other features
+const newStringOne = "  farhan   "
+console.log(newStringOne.trim());
+
+
+//Replace
+
+cons url = "https:/Farhan.com/farhanajum20"
+
+console.log(url.replace('%20', '-'))
+
+
+console.log(url.includes('Farhan'));
+
+
+//Split method
+
+console.log(getName.split('-');
+
+
+
+//refere MDN docs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
