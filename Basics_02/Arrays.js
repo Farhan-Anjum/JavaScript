50ed6c65ec11166ab9c78c5a534b53e95661da34
Farhan Anjum <your.email@example.com>
2023-12-17 11:01:58 +0500

    Arrays Done

--- Basics_02/Arrays.js
+++ Basics_02/Arrays.js
@@ -60,12 +60,41 @@ console.log(myn2);
 
 //Difference:Change the original array or Manipulate the original array - take out the portio
 
+const marvel_heroes = ["thor", "Ironman", "Spiderman"]
+const dc_heroes = ["Superman", "Flash", "Batman"]
+
+marvel_heroes.push(dc_heroes)
+console.log(marvel_heroes); // will create a array within array
+//make array as an element
 
 
+const all_heroes = marvel_heroes.concat(dc_heroes);
+console.log(all_heroes); //merge the elements
 
 
+const all_new_heroes = [...marvel_heroes, ...dc_heroes] //Spread operator
+console.log(all_new_heroes); //erge the elements
+
+//Flat operator
+const another_array = [1, 2,3,[4,5,6], 7, [6,7,[4,5]]]
+                            
+ const real_another_array = another_array.flat(Infinity) //can pass any depth
+console.log(real_another_array);
 
 
+//Important
 
+console.log(Array.isArray("Farhan"));
+console.log(Array.from("Farhan")); //make separate array of the each character
+
+console.log(Array.from({name :"Farhan"})); //Best for Data conversion or formating
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 400
+
+
+console.log(Array.of(score1, score2, score3)); //convert into the array
 
 
